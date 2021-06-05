3a5381abc2af743f3e145754c544f5716059876f
Deva88 <devendra.raj.sdm@gmail.com>
2021-06-05 05:31:11 +0530

    Added: Integer Operations

---
+++ src/com/bridgelabz/bootcampJava/day1/IntOpt.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.bootcampJava.day1;
+
+import java.util.Scanner;
+
+import com.bridgelabz.bootcampJava.util.Utility;
+
+/*
+Write a IntOpt.java program by taking a, b and c as input values and print the
+following integer operations 
+a + b *c            a * b + c 
+c + a / b           a % b + c.
+also understand the precedence of the operators.
+*/
+
+public class IntOpt {
+
+	public static void main(String[] args) {
+		int a, b,c;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a vale : ");
+		a = sc.nextInt();
+		System.out.println("Enter b value : ");
+		b=sc.nextInt();
+		System.out.println("Enter c value : ");
+		c=sc.nextInt();
+		
+		Utility.mathOpt(a, b, c);
+		
+		
+	}
+}
--- src/com/bridgelabz/bootcampJava/util/Utility.java
+++ src/com/bridgelabz/bootcampJava/util/Utility.java
@@ -2,4 +2,19 @@ package com.bridgelabz.bootcampJava.util;
 
 public class Utility {
 
+	//Day-1
+	
+	public static void mathOpt(int a, int b, int c) {
+		//int a=10, b=20, c=30;
+		int operation1= a + b * c;
+		int operation2= a * b + c;
+		int operation3= c + a / b;
+		int operation4= a % b + c;
+		
+		System.out.println("Operation one (a + b * c): "+operation1);
+		System.out.println("Operation two (a * b + c): "+operation2);
+		System.out.println("Operation Three (c + a / b): "+operation3);
+		System.out.println("Operation Four (a % b + c): "+operation4);	
+	}
+
 }
