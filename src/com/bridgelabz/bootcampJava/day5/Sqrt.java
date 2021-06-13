aab8b83200badaae3ceced5d8ebe1991bd6e11eb
Deva88 <devendra.raj.sdm@gmail.com>
2021-06-13 16:48:02 +0530

    Added: Return the square root of Number Using Newton's method

---
+++ src/com/bridgelabz/bootcampJava/day5/Sqrt.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.bootcampJava.day5;
+
+import java.util.Scanner;
+import com.bridgelabz.bootcampJava.util.Utility;
+public class Sqrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Utility utility=new Utility();
+		Scanner scanner = new Scanner(System.in);
+		int c;
+		double result;
+		System.out.println("Enter value");
+		c = scanner.nextInt();
+		if (c > 0) {
+			result = utility.findSquareRootUsingNewtonsMethod(c);
+			System.out.println("Square Root of " + c + " using Newtons Law is " + result);
+		} else {
+			System.out.println("Please enter positive value..");
+		}
+	}
+}
