93b03f0b2ba732d210a2a3c2429a9d303bcc4a12
Deva88 <devendra.raj.sdm@gmail.com>
2021-06-16 06:20:18 +0530

    Added: check prime Number or not

--- src/com/bridgelabz/bootcampJava/day5/PrimeNumber.java
+++ src/com/bridgelabz/bootcampJava/day5/PrimeNumber.java
@@ -3,7 +3,6 @@ package com.bridgelabz.bootcampJava.day5;
 import com.bridgelabz.bootcampJava.util.MathFunction;
 
 public class PrimeNumber {
-
 	public static void main(String[] args) {
 		for (int i = 0; i <= 1000; i++) {
 			if (MathFunction.isPrime(i)) {
