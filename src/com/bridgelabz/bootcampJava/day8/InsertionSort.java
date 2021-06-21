e064d7568f36dbf07e6d0d01ebb4095932fba556
Deva88 <devendra.raj.sdm@gmail.com>
2021-06-21 06:27:15 +0530

    Added: take one Array and sort Using Insertion sort

--- src/com/bridgelabz/bootcampJava/day8/InsertionSort.java
+++ src/com/bridgelabz/bootcampJava/day8/InsertionSort.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.bootcampJava.day8;
 import com.bridgelabz.bootcampJava.util.SortingSearch;
-
 public class InsertionSort {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] intArr = { 1, 5, 2, 6, 7, 9, 3 };
@@ -10,8 +10,5 @@ public class InsertionSort {
 		SortingSearch.intInsertionSort(intArr);
 		System.out.println("After sorting");
 		SortingSearch.printIntArr(intArr);
-		
-
 	}
-
 }
