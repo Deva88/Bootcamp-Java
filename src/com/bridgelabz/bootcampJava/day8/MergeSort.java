0125230e5c9832f91b7b74842bbd1223515419e0
Deva88 <devendra.raj.sdm@gmail.com>
2021-06-21 06:28:11 +0530

    Added: take one Array and sort Using Merge sort

--- src/com/bridgelabz/bootcampJava/day8/MergeSort.java
+++ src/com/bridgelabz/bootcampJava/day8/MergeSort.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.bootcampJava.day8;
 import com.bridgelabz.bootcampJava.util.SortingSearch;
-
 public class MergeSort {
+	
 	public static void main(String[] args) {
 		SortingSearch sortingsearch=new SortingSearch();
 		String[] arr= {"deva","abhi","devendra","rock","ravi","lio"};
 		int start=0,end=arr.length-1;
 		sortingsearch.mergeSort(arr,start,end);
 		SortingSearch.printStringArr(arr);
-
 	}
 }
